ce7b61d26c3453358eda7075486cf0175c58c526
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-04-22 14:10:46 +0300

    chore: migrate to bump-my-version

--- src/matrixrmapi/__init__.py
+++ src/matrixrmapi/__init__.py
@@ -1,3 +1,3 @@
 """Matrix product RASENMAEHER integration API service"""
 
-__version__ = "1.0.2"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.0.2-260422"  # NOTE Use `bump-my-version bump release` to bump versions correctly
