12f19ba9dee2ecf4d426af56b00ab3f0867c342f
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-05-12 13:51:20 +0300

    chore: bump version to 1.1.1+260512

--- tests/test_matrixrmapi.py
+++ tests/test_matrixrmapi.py
@@ -6,7 +6,7 @@ from matrixrmapi import __version__
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.1.0+260512"
+    assert __version__ == "1.1.1+260512"
 
 
 def test_healthcheck(mtlsclient: TestClient) -> None:
